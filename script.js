3998f3e81bdd8fa0b5252820de716f471892f275
Swethashivani <40227788+Swethashivani@users.noreply.github.com>
2025-03-20 17:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const outputDiv = document.getElementById("output");
+
+function delayedPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]); // Initial array after 3 seconds
+        }, 3000);
+    });
+}
+
+delayedPromise()
+    .then((arr) => {
+        // First transformation: Filter even numbers
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const evenNumbers = arr.filter(num => num % 2 === 0);
+                outputDiv.innerText = `[${evenNumbers.join(", ")}]`; // Update output div
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then((evenNumbers) => {
+        // Second transformation: Multiply by 2
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const multipliedNumbers = evenNumbers.map(num => num * 2);
+                outputDiv.innerText = `[${multipliedNumbers.join(", ")}]`; // Update output div again
+                resolve(multipliedNumbers);
+            }, 2000);
+        });
+    });
+
